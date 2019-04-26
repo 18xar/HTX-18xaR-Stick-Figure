7130452da8116457673828a305905909acc4bcdc
OttoJHTX <sejeotto@gmail.com>
2019-04-26 09:42:02 +0200

    Tilføjet sort baggrund

--- OttoJ.py
+++ OttoJ.py
@@ -3,6 +3,7 @@ def OttoJ(x, y):
     xCenter = x + 20
     yCenter = y + 40
 
+    arcade.draw_rectangle_filled(x + 20, y + 40, 40, 80, arcade.color.BLACK)
     arcade.draw_line(x, y, x + 40, y + 80, (255, 255, 255), 10)
     arcade.draw_line(x + 40, y, x, y + 80, (255, 255, 255), 10)
     arcade.draw_circle_filled(xCenter, yCenter - 15, 5, (255, 0, 8))
