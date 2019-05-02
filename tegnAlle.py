b724eb9672ecd7c87c412e7cbca819139c3f1378
NikolajK-HTX <kagenogmig@gmail.com>
2019-05-02 12:41:27 +0200

    tilføjet kommentarer

--- tegnAlle.py
+++ tegnAlle.py
@@ -1,3 +1,4 @@
+# Henter funktionerne fra alle de forskellige filer
 import arcade
 from NikolajK import *
 from SamuelS import *
@@ -17,9 +18,11 @@ arcade.set_background_color(arcade.color.WHITE)
 
 arcade.start_render()
 
+# Liste med alle de funktioner som kan vælges imellem.
 ListMedFunktioner = [NikolajK, dreng, pige, AndreasP, LuccasS1, LuccasS2, AslakD, VictorC, MartinP, ThomasS, ChristianW,
                      OttoJ, KasperEBP]
 
+# Kører igennem alle koordinatsættene på skærmen.
 for x in range(0, 600, 40):
     for y in range(0, 400, 80):
         valg = random.choice(ListMedFunktioner)
