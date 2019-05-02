c7a3aab832dc2157188647519989ce4bd9670caf
OttoJHTX <sejeotto@gmail.com>
2019-05-02 13:22:17 +0200

    Fixet det hvide kryds' overlap med andres stickfigures

--- OttoJ.py
+++ OttoJ.py
@@ -4,8 +4,8 @@ def OttoJ(x, y):
     yCenter = y + 40
 
     arcade.draw_rectangle_filled(x + 20, y + 40, 40, 80, arcade.color.BLACK)
-    arcade.draw_line(x, y, x + 40, y + 80, (255, 255, 255), 10)
-    arcade.draw_line(x + 40, y, x, y + 80, (255, 255, 255), 10)
+    arcade.draw_line(x + 5, y + 5, x + 35, y + 75, (255, 255, 255), 10)
+    arcade.draw_line(x + 35, y + 5, x + 5, y + 75, (255, 255, 255), 10)
     arcade.draw_circle_filled(xCenter, yCenter - 15, 5, (255, 0, 8))
     arcade.draw_triangle_filled(xCenter, yCenter + 10, xCenter - 8, yCenter - 10, xCenter + 8, yCenter - 10, (255, 0, 8))
     arcade.draw_line(xCenter - 8, yCenter - 10, xCenter - 12, yCenter - 25, (255, 0, 8), 2)
