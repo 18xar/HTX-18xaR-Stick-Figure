089e083c32cb936af437068446a9f4f1f4103e9c
NikolajK-HTX <kagenogmig@gmail.com>
2019-05-02 13:23:15 +0200

    ændret og tilføjet martins funktion så den virker.

--- tegnAlle.py
+++ tegnAlle.py
@@ -24,8 +24,9 @@ arcade.set_background_color(arcade.color.WHITE)
 arcade.start_render()
 
 # Liste med alle de funktioner som kan vælges imellem.
-ListMedFunktioner = [NikolajK, dreng, pige, AndreasP, LuccasS1, LuccasS2, AslakD, VictorC, MartinP, ThomasS, ChristianW,
-                     OttoJ, KasperEBP, DanielP1, DanielP2, OskarK, KristofferW, LauritsHMan1, LauritsHMan2, BusterD]
+ListMedFunktioner = [NikolajK, dreng, pige, AndreasP, LuccasS1, LuccasS2, AslakD, VictorC, ThomasS, ChristianW,
+                     OttoJ, KasperEBP, DanielP1, DanielP2, OskarK, KristofferW, LauritsHMan1, LauritsHMan2, BusterD,
+                     MartinP]
 
 # Kører igennem alle koordinatsættene på skærmen.
 for x in range(0, 600, 40):
