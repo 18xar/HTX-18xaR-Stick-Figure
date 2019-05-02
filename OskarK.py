48e97e2ffdf90b58b1ab7e3140a957266a3146f9
oskarkragh <kraghoskar@gmail.com>
2019-05-02 12:55:23 +0200

    det ser fint

--- OskarK.py
+++ OskarK.py
@@ -9,21 +9,21 @@ def OskarK(x,y):
     arcade.open_window(w, h, "stikfigure")
     arcade.set_background_color((0, 0, 255))
 
-    arcade.draw_rectangle_filled(w/2+x, h/2+y, w, h/2,(60,63,200))
+    arcade.draw_rectangle_filled(w/2+x, h/2+y, w, h/2,(100,173,200))
 
 
 
-    arcade.draw_circle_filled(w/2+x, h*0.8+y, w/8,(0,0,0))
+    arcade.draw_circle_filled(w/2+x, h*0.6+y, w/8,(0,0,0))
 
     #krop
-    arcade.draw_line(w/2+x, h*0.8+y, w/2+x, h*0.3+y,(0,0,0), w/16)
+    arcade.draw_line(w/2+x, h*0.6+y, w/2+x, h*0.35+y,(0,0,0), w/16)
 
     #arme
-    arcade.draw_line(w/2+x, h*0.7+y, w/5+x, h*0.9+y,(0,0,0), w/16)
+    arcade.draw_line(w/2+x, h*0.5+y, w/5+x, h*0.6+y,(0,0,0), w/16)
 
     #ben
-    arcade.draw_line(w/2+x, h*0.3+y, w*0.6+x, h*0.05+y,(0,0,0), w/16)
-    arcade.draw_line(w/2+x, h*0.3+y, w*0.4+x, h*0.05+y,(0,0,0), w/16)
+    arcade.draw_line(w/2+x, h*0.35+y, w*0.65+x, h*0.1+y,(0,0,0), w/20)
+    arcade.draw_line(w/2+x, h*0.35+y, w*0.35+x, h*0.1+y,(0,0,0), w/20)
 
     #telefon
     arcade.draw_line(w/2+x, h*0.3+y, w*0.33+x, h*0.3+y,(0,0,0), w/16)
